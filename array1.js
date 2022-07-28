a67520547521a2b63bec1fbb84d94fe56fbb6038
vinislima <vinislima@gmail.com>
2022-07-28 15:53:14 -0300

    estudo

---
+++ array1.js
@@ -0,0 +1,7 @@
+const arr = [1, 5 , 10, "ola", true];
+
+let apenasNumero = arr.every(function (){
+    return
+}) 
+
+console.log
---
+++ reduce.js
@@ -0,0 +1,12 @@
+
+const numeros = [1, 3, 4, 1, 4, 5, 3, 5, 8, 9];
+
+let numerosUnicos = numeros.reduce(function(numerosUnicos, numeroAtual){
+
+     if (numerosUnicos.indexOf(numeroAtual) < 0) {
+        numerosUnicos.push(numeroAtual);
+     }
+     return numerosUnicos;
+}, []);
+console.log(numerosUnicos);
+
