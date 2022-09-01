70d7ef7f4907ffc57f0f13101291529c8bb08311
vinislima <vinislima@gmail.com>
2022-09-01 14:41:00 -0300

    estudo

--- to_do_list/todolist.js
+++ to_do_list/todolist.js
@@ -5,21 +5,35 @@
     const itemInput = document.getElementById("item-input");
     const toDoAddForm = document.getElementById("todo-add");
     const ul = document.getElementById("todo-list");
-    // const lis = ul.getElementsByTagName("li");
+    const lis = ul.getElementsByTagName("li");
 
-    let arrTasks = [
-        {
-            name: "task 1",
-            createAt: Date.now(),
-            completed: false
-        }
-    ];
+    let arrTasks = getSavedData();
 
     // function addEventLi(li) {
     //     li.addEventListener("click", function(){
     //         console.log(this);
     //     });
     // }
+
+    function getSavedData() {
+        let tasksData = localStorage.getItem("tasks");
+        tasksData = JSON.parse(tasksData);
+
+        return tasksData && tasksData.length ? tasksData : [
+            {
+                name: "task 1",
+                createAt: Date.now(),
+                completed: false
+            }
+        ]
+    }
+
+    function setNewData() {
+
+        localStorage.setItem("tasks", JSON.stringify(arrTasks));
+    }
+
+    setNewData();
 
     function generateLiTask(obj) {
         const li = document.createElement("li");
@@ -31,7 +45,8 @@
         li.className = "todo-item";
 
         checkButton.className = "button-check";
-        checkButton.innerHTML = '<i class="fas fa-check displayNone"></i>'
+        checkButton.innerHTML = `
+            <i class="fas fa-check ${obj.completed ? "" : "displayNone"}" data-action="checkButton"></i>`
         checkButton.setAttribute("data-action", "checkButton")
         
         li.appendChild(checkButton);
@@ -49,6 +64,7 @@
         const inputEdit = document.createElement("input");
         inputEdit.setAttribute("type", "text");
         inputEdit.className = "editInput";
+        inputEdit.value = obj.name;
         containerEdit.appendChild(inputEdit);
         const containerEditButton = document.createElement("button");
         containerEditButton.className = "editButton";
@@ -58,7 +74,7 @@
         const containerCancelButton = document.createElement("button");
         containerCancelButton.className = "cancelButton";
         containerCancelButton.textContent = "Cancel";
-        containerCancelButton.setAttribute("data-actio", "containerCancelButton");
+        containerCancelButton.setAttribute("data-action", "containerCancelButton");
         containerEdit.appendChild(containerCancelButton);
 
         li.appendChild(containerEdit);
@@ -70,14 +86,14 @@
 
         // addEventLi(li);
         return li;
-    }
+    };
 
     function renderTask(){
         ul.innerHTML = "";
         arrTasks.forEach(taskObj => {
             ul.appendChild(generateLiTask(taskObj))
         });
-    }
+    };
 
 
     function addTask(task) {
@@ -86,12 +102,71 @@
             name: task,
             createAt: Date.now(),
             completed: false
-        })
-    }
+        });
+
+        setNewData();
+    };
 
     function clickedUl(e) {
-        console.log(e.target);
-        console.log(e.target.getAttribute("data-action"));
+        const dataAction = e.target.getAttribute("data-action")
+        if(!dataAction) return;
+
+        let currentLi = e.target;
+        while(currentLi.nodeName !== "LI") {
+            currentLi = currentLi.parentElement;
+        }
+
+        const currentLiIndex = [...lis].indexOf(currentLi);
+
+        const actions = {
+            editButton: function() {
+                const editContainer = currentLi.querySelector(".editContainer");
+                
+                [...ul.querySelectorAll(".editContainer")].forEach(container => {
+                    container.removeAttribute("style");
+                });
+
+                editContainer.style.display = "flex";
+                
+            },
+
+            deleteButton: function() {
+                arrTasks.splice(currentLiIndex, 1);
+                console.log(arrTasks);
+                renderTask();
+                setNewData();
+            },
+
+            containerEditButton: function() {
+                const val = currentLi.querySelector(".editInput").value;
+                arrTasks[currentLiIndex].name = val;
+                renderTask();
+                setNewData();
+            },
+
+            containerCancelButton: function() {
+                currentLi.querySelector(".editContainer").removeAttribute("style");
+                currentLi.querySelector(".editInput").value = arrTasks[currentLiIndex].name;
+            },
+
+            checkButton: function() {
+                arrTasks[currentLiIndex].completed = !arrTasks[currentLiIndex].completed;
+
+                if(arrTasks[currentLiIndex].completed){
+
+                    currentLi.querySelector(".fa-check").classList.remove("displayNone");
+                } else {
+                    currentLi.querySelector(".fa-check").classList.add("displayNone");
+                }
+                
+                setNewData();
+                renderTask();
+            }
+        }
+
+        if (actions[dataAction]) {
+            actions[dataAction]()
+        }
     }
 
     toDoAddForm.addEventListener("submit", function(e) {
