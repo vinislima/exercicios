35a68ff90decb26b34971093d64c425159061076
vinislima <vinislima@gmail.com>
2022-09-14 17:22:27 -0300

    estudo

--- geral/replaceAll.js
+++ geral/replaceAll.js
@@ -2,6 +2,16 @@ if (String.prototype.replaceAll) {
     
     String.prototype.replaceAll = function(busca, troca) {
 
+        if (!(busca instanceof String || typeof busca === "string")){
+
+            throw Error("Informar uma string")
+        }
+
+        if (!(troca instanceof String || typeof troca === "string")){
+
+            throw Error("Informar uma string")
+        }
+
         return this.valueOf().split(busca).join(troca);
     }
 }
